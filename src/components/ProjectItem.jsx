786823e91140d1242d2f1800870a63bbf2c21d1d
Bismark Arroyo <bis.arroyo@gmail.com>
2023-02-01 00:07:40 -0600

    added project and transition effect

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,45 +1,35 @@
 import { AiFillGithub, AiOutlineLink } from 'react-icons/ai'
-import { useInView } from 'framer-motion'
-import { useRef } from 'react'
 
 const ProjectItem = ({ projects }) => {
-  const ref = useRef(null)
-  const isInView = useInView(ref, { once: true })
   return (
     <>
-      {projects.map((i) => (
+      {projects.map((project, i) => (
         <div
           className='project-container flex-column'
-          key={i.id}
-          ref={ref}
-          style={{
-            transform: isInView ? 'none' : 'translateY(50px)',
-            opacity: isInView ? 1 : 0,
-            transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s'
-          }}
+          key={project.id}
         >
           <div className='project-image'>
-            <img src={i.image} alt='' />
+            <img src={project.image} alt='' />
           </div>
           <div className='project-card'>
             <div className='project-title'>
-              <h6>{i.name}</h6>
+              <h6>{project.name}</h6>
             </div>
             <div className='project-content'>
-              <p>{i.description}</p>
+              <p>{project.description}</p>
             </div>
             <div className='project-info-container flex-row'>
               <div className='project-tags flex-row'>
-                <p>{i.tags}</p>
+                <p>{project.tags}</p>
               </div>
               <div className='project-links flex-row'>
                 <div className='project-link-page'>
-                  <a href={i.link} target='_blank' rel='noreferrer'>
+                  <a href={project.link} target='_blank' rel='noreferrer'>
                     <AiOutlineLink />
                   </a>
                 </div>
                 <div className='project-link-github'>
-                  <a href={i.gitUrl} target='_blank' rel='noreferrer'>
+                  <a href={project.gitUrl} target='_blank' rel='noreferrer'>
                     <AiFillGithub />
                   </a>
                 </div>
--- src/containers/HomeProjects.jsx
+++ src/containers/HomeProjects.jsx
@@ -1,9 +1,21 @@
 import ProjectItem from '../components/ProjectItem'
 import projects from '../services/projects'
+import { useInView } from 'framer-motion'
+import { useRef } from 'react'
 
 const HomeProjects = () => {
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
   return (
-    <div className='projects'>
+    <div
+      className='projects'
+      ref={ref}
+      style={{
+        transform: isInView ? 'none' : 'translateY(50px)',
+        opacity: isInView ? 1 : 0,
+        transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s'
+      }}
+    >
       <h2>Projects</h2>
       <div className='flex-fluid'>
         <ProjectItem projects={projects} />
